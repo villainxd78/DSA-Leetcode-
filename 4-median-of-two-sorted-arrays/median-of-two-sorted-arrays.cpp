705b0b1f23300e43a9ac88aabc70010b8f915b5d
villain xd <kunalrawat591@gmail.com>
2026-09-18 10:49:30 +0530

    Time: 0 ms (100.00%) | Memory: 94.9 MB (94.60%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,40 +1,69 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m = nums1.size();
-        int n = nums2.size();
+        int n = nums1.size();
+        int m = nums2.size();
 
-        vector<int>temp(m+n);
+        int size = m+n;
+        int index1 = size/2;
+        int element1 = -1;
+        int index2 = size/2 - 1;
+        int element2 = -1;
         int i = 0;
         int j = 0;
         int k = 0;
-        while(i<m&& j<n){
-        if(nums1[i]<nums2[j]){
-            temp[k]=nums1[i];
+
+        while(i<n&&j<m){
+            if(nums1[i]<nums2[j]){
+                if(k ==  index1){
+                    element1 = nums1[i];
+                }
+                 if(k==index2){
+               element2 = nums1[i];
+                }
+                i++;
+            }
+                   
+            else{
+                if(k==index1){
+                    element1 = nums2[j];
+
+                }
+                if(k==index2){
+                    element2 = nums2[j];
+                }
+                j++;
+            }
             k++;
+        }
+        while(i<n){
+            if(k==index1){
+                element1 = nums1[i];
+            }
+            if(k==index2){
+                element2 = nums1[i];
+            }
             i++;
-        }
-        else{
-            temp[k]=nums2[j];
             k++;
+        }
+        while(j<m){
+            if(k==index1){
+                element1 = nums2[j];
+
+            }
+            if(k==index2){
+                element2 = nums2[j];
+
+            }
             j++;
-        }
-        }
-        while(i<m){
-            temp[k]=nums1[i];
             k++;
-            i++;
         }
-        while(j<n){
-            temp[k]=nums2[j];
-            k++;
-            j++;
-        }
-        int size = m+n;
-        if(size%2==1){
-            return temp[size/2];
-            
-        }
-        return (temp[size/2] + temp[size/2-1])/ 2.0;
+        if(size % 2 == 1)
+            return element1;
+
+        return (element1 + element2) / 2.0;
+
+
+
     }
 };
