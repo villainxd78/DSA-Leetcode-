b80e4925c652d87f57ecc787d292ea8338bd0e8c
villain xd <kunalrawat591@gmail.com>
2026-09-25 17:06:38 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (70.37%) - LeetSync

---
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+   long long pow(long long a,long long b){
+    long long m = 1000000007;
+    if(b == 0) return 1;
+    long long temp = pow(a,b/2);
+    if(b%2 ==0){
+        return (temp * temp)%m;
+    }else{
+        return ((temp* temp)%m * a)%m;
+    }
+    
+        
+    }
+   
+    int countGoodNumbers(long long n) {
+        long long m = 1000000007;
+        return (pow(5,(n+1)/2) * pow(4,n/2))%m;
+    }
+};
